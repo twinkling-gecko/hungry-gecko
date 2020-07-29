368a15b101ea3641ab5594e57303e9f4277d0e52
sota suzuki <k017c1356@it-neec.jp>
2020-07-29 17:37:28 +0900

    feat: add item detail pages

---
+++ src/tangerine/components/ItemDetail.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h2>{{item.name}}</h2>
+    <p>{{item.summary}}</p>
+    <BLink :href="item.uri">{{item.uri}}</BLink>
+    <p>更新日：{{item.updated_at}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { BLink } from 'bootstrap-vue'
+
+interface Item {
+  created_at: string
+  id: number
+  name: string
+  summary: string
+  updated_at: string
+  uri: string
+}
+
+@Component({
+  components: {
+    BLink,
+  },
+})
+export default class ItemDetail extends Vue {
+  @Prop({ type: Object as PropType<Item>, default: {} })
+  item!: Item
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 8px;
+}
+</style>
---
+++ src/tangerine/pages/detail/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ItemDetail :item="data" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import ItemDetail from '@/components/ItemDetail.vue'
+
+@Component({
+  components: {
+    ItemDetail,
+  },
+})
+export default class Detail extends Vue {
+  async asyncData(context: any) {
+    const id = context.route.params.id
+    const res = await context.$axios.get('http://localhost/api/v1/items/' + id)
+    console.log(res)
+    return {
+      data: res.data,
+    }
+  }
+}
+</script>
