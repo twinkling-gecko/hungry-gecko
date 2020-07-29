860a9895d5d905b53ddf2d9ce3fdd45280e1e6c8
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-07-29 13:37:39 +0900

    test: itemsCreateRouterのテストを実装

--- src/macksnow/pkg/router/items_test.go
+++ src/macksnow/pkg/router/items_test.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"bytes"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -44,4 +45,54 @@ func TestItemsShowRouter(t *testing.T) {
 
 	assert.Equal(t, http.StatusBadRequest, invalidRec.Code)
 	assert.Equal(t, "{\"message\":\"hoge is not integer.\"}\n", string(body))
+}
+
+func TestItemsCreateRouter(t *testing.T) {
+	e := echo.New()
+	itemsCreateRouter(e)
+
+	// 正しいパラメータでリクエストをした場合
+	json := `{"name":"hoge","summary":"fuga","uri":"piyo"}`
+	params := bytes.NewBuffer([]byte(json))
+
+	validReq := httptest.NewRequest("POST", "/v1/items", params)
+	validReq.Header.Set("Content-Type", "application/json")
+	validRec := httptest.NewRecorder()
+
+	e.ServeHTTP(validRec, validReq)
+	assert.Equal(t, http.StatusOK, validRec.Code)
+
+	// 構文エラーのパラメータでリクエストをした場合
+	json = `{"name""hoge","summary":"fuga","uri":"piyo"}`
+	params = bytes.NewBuffer([]byte(json))
+
+	invalidReq := httptest.NewRequest("POST", "/v1/items", params)
+	invalidReq.Header.Set("Content-Type", "application/json")
+	invalidRec := httptest.NewRecorder()
+	e.ServeHTTP(invalidRec, invalidReq)
+
+	body, err := ioutil.ReadAll(invalidRec.Result().Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, invalidRec.Code)
+	assert.Equal(t, "{\"message\":\"Invalid parameters.\"}\n", string(body))
+
+	// 必須パラメータが不足したリクエストをした場合
+	json = `{"name":"","summary":"fuga","uri":"piyo"}`
+	params = bytes.NewBuffer([]byte(json))
+
+	incompleteReq := httptest.NewRequest("POST", "/v1/items", params)
+	incompleteReq.Header.Set("Content-Type", "application/json")
+	incompleteRec := httptest.NewRecorder()
+	e.ServeHTTP(incompleteRec, incompleteReq)
+
+	body, err = ioutil.ReadAll(incompleteRec.Result().Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, incompleteRec.Code)
+	assert.Equal(t, `{"message":"Required parameters is empty. Key: 'item.Name' Error:Field validation for 'Name' failed on the 'required' tag"}`+"\n", string(body))
 }
