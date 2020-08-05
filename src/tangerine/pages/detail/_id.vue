79b3408a2f963d4b2ab5c4091f7a177c658a8baa
Sota Suzuki <k017c1356@it-neec.jp>
2020-08-05 12:26:00 +0900

    feat: catch detail fetch error

--- src/tangerine/pages/detail/_id.vue
+++ src/tangerine/pages/detail/_id.vue
@@ -1,5 +1,6 @@
 <template>
-  <ItemDetail :item="data" />
+  <ItemDetail v-if="!fetchFail" :item="item" />
+  <div v-else>通信に失敗しました</div>
 </template>
 
 <script lang="ts">
@@ -8,18 +9,42 @@ import ItemDetail from '@/components/ItemDetail.vue'
 
 @Component({
   components: {
-    ItemDetail
-  }
+    ItemDetail,
+  },
+  asyncData(context: any) {
+    const id = context.route.params.id
+    return context.$axios
+      .get(context.$axios.defaults.baseURL + 'items/' + id)
+      .then((res: any) => {
+        const data = res.data
+        return {
+          item: {
+            id: data.id,
+            name: data.name,
+            summary: data.summary,
+            uri: data.uri,
+            created_at: data.created_at,
+            updated_at: data.updated_at,
+          },
+        }
+      })
+      .catch(() => {
+        return {
+          fetchFail: true,
+        }
+      })
+  },
 })
 export default class Detail extends Vue {
-  async asyncData(context: any) {
-    const id = context.route.params.id
-    const res = await context.$axios.get(
-      context.$axios.defaults.baseURL + 'items/' + id
-    )
-    return {
-      data: res.data
-    }
+  item = {
+    id: 0,
+    name: '',
+    summary: '',
+    uri: '',
+    created_at: Date.now(),
+    updated_at: Date.now(),
   }
+
+  fetchFail = false
 }
 </script>
---
+++ src/tangerine/types/index.d.ts
@@ -0,0 +1,8 @@
+export interface Item {
+  id: number
+  name: string
+  summary: string
+  uri: string
+  created_at: number
+  updated_at: number
+}
