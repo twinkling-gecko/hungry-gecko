97c127b8c05eb45d0fc6ebb1fd7eeebeee89b702
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-08-27 14:40:43 +0900

    refact: to apply html5 form

--- src/tangerine/pages/items/new.vue
+++ src/tangerine/pages/items/new.vue
@@ -1,14 +1,40 @@
 <template>
-  <div>
-    <p>商品登録画面</p>
-    <p>商品名</p>
-    <input v-model="title" placeholder="商品名を入力してね" />
-    <p>商品概要</p>
-    <input v-model="overview" placeholder="商品概要を入力" />
-    <p>商品リンク</p>
-    <input v-model="link" placeholder="商品のリンクを貼り付けるところ" />
-    <button @click="onClickButton">登録</button>
-  </div>
+  <b-container>
+    <h1>商品登録画面</h1>
+
+    <b-container>
+      <b-form @submit="onSubmit" @reset="onReset">
+        <b-form-group label="商品名" label-for="item-name">
+          <b-form-input
+            v-model="form.title"
+            id="item-name"
+            type="text"
+            placeholder="商品名を入力してね"
+            required
+          />
+        </b-form-group>
+        <b-form-group label="商品概要" label-for="item-summary">
+          <b-form-input
+            v-model="form.summary"
+            id="item-summary"
+            type="text"
+            placeholder="商品概要を入力してね"
+            required
+          />
+        </b-form-group>
+        <b-form-group label="商品リンク" label-for="item-link">
+          <b-form-input
+            v-model="form.link"
+            id="item-link"
+            type="text"
+            placeholder="商品のリンクを入力してね"
+            required
+          />
+        </b-form-group>
+        <b-button type="submit">登録</b-button>
+      </b-form>
+    </b-container>
+  </b-container>
 </template>
 
 <script lang="ts">
@@ -17,43 +43,33 @@ import { Component, Vue } from 'nuxt-property-decorator'
 @Component
 export default class ItemList extends Vue {
   // DATAの定義
-  title = ''
-  overview = ''
-  link = ''
-
-  // クリック時のアクション
-  onClickButton() {
-    if (this.validateSendItem()) {
-      this.$axios
-        .post(this.$axios.defaults.baseURL + 'items', {
-          name: this.title,
-          summary: this.overview,
-          uri: this.link,
-        })
-        .then(() => {
-          alert('登録完了しました。')
-          this.clearItemForm()
-        })
-        .catch(() => {
-          alert('登録失敗')
-        })
-    } else {
-      alert('登録情報を入力してください')
-    }
+  form = {
+    title: '',
+    summary: '',
+    link: '',
   }
 
-  validateSendItem() {
-    if (this.title && this.overview && this.link) {
-      return true
-    } else {
-      return false
-    }
+  // submit時のアクション
+  onSubmit() {
+    this.$axios
+      .post(this.$axios.defaults.baseURL + 'items', {
+        name: this.form.title,
+        summary: this.form.summary,
+        uri: this.form.link,
+      })
+      .then(() => {
+        alert('登録完了しました。')
+        this.onReset()
+      })
+      .catch(() => {
+        alert('登録失敗')
+      })
   }
 
-  clearItemForm() {
-    this.title = ''
-    this.overview = ''
-    this.link = ''
+  onReset() {
+    this.form.title = ''
+    this.form.summary = ''
+    this.form.link = ''
   }
 }
 </script>
