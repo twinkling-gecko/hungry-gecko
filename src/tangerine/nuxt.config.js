b83ec1821ce3f261b9f511bc082e581e28d0aea1
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-09-15 13:36:40 +0900

    feat: define AXIOS_BASE_URL einvironment variable on tangerine

--- src/tangerine/nuxt.config.js
+++ src/tangerine/nuxt.config.js
@@ -64,7 +64,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'http://localhost/api/v1/',
+    baseURL: process.env.AXIOS_BASE_URL,
   },
   /*
    ** Build configuration
