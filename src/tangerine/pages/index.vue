e2fc43cadf1832e190e7ff6bd432ba4a5a4147dc
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-10-08 12:39:09 +0900

    feat: add control button to index page

--- src/tangerine/pages/index.vue
+++ src/tangerine/pages/index.vue
@@ -5,6 +5,19 @@
       <Logo />
       <h1 class="display-1">Hungry Gecko</h1>
     </div>
+    <div class="text-center">
+      <b-button class="mr-2" variant="outline-primary" size="lg" to="/items">
+        ItemList
+      </b-button>
+      <b-button
+        class="ml-2"
+        variant="outline-secondary"
+        size="lg"
+        to="/items/new"
+      >
+        NewItem
+      </b-button>
+    </div>
   </b-container>
 </template>
 
