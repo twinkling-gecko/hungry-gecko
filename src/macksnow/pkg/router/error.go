bc5ab3d3a07052fd20f3c605fba2aea5347108ef
Sota Suzuki <k017c1356@it-neec.jp>
2020-10-08 16:57:32 +0900

    WIP: add user router

---
+++ src/macksnow/pkg/router/error.go
@@ -0,0 +1,5 @@
+package router
+
+type errorResponse struct {
+	Message string `json:"message"`
+}
--- src/macksnow/pkg/router/users.go
+++ src/macksnow/pkg/router/users.go
@@ -1,14 +1,16 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
 type receiveUser struct {
-	Nickname   string `json:"nickname" validate: "required"`
-	ScreenName string `json:"screen_name" validate: "required"`
-	Email      string `json:"email" validate: "required"`
-	Password   string `json:"password" validate: "required"`
+	Nickname   string `json:"nickname" validate: "required,min=4,max=15"`
+	ScreenName string `json:"screen_name" validate: "required,alphanum,min=4,max=15"`
+	Email      string `json:"email" validate: "required,email"`
+	Password   string `json:"password" validate: "required,containsany=!@#$%^&*()-=_+[]{}\|;':",.<>/?,min=8,max=20"`
 }
 
 // @summary Register user info
@@ -19,9 +21,24 @@ type receiveUser struct {
 // @router /api/v1/users [post]
 func usersCreateRouter(e *echo.Echo) {
 	e.POST("/v1/users", func(c echo.Context) error {
-		// リクエストバインド
+
+		receivedUser := new(receiveUser)
+
+		// リクエストのバインド
+		if err := c.Bind(receivedUser); err != nil {
+			res := &errorResponse{Message: "Invalid parameters."}
+			return c.JSON(http.StatusBadRequest, res)
+		}
+
 		// リクエストのバリデート
-		// パスワードハッシュ
+		if err := c.Validate(receivedUser); err != nil {
+			// TODO: 不正パラメータの原因を通知する
+			res := &errorResponse{Message: "The requested parameter is invalid." + err.Error()}
+			return c.JSON(http.StatusBadRequest, res)
+		}
+
+		// パスワードハッシュの生成
+		// confirmation_tokenの生成
 		// Userモデルを起こす
 		// Userモデルバリデート
 		// CreateUser
